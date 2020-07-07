33ff7fd810768a08cb880714a3c6911843bcf843
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-07-07 12:40:48 -0400

    moved react router from index to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Row from './components/Row';
+import Login from './components/Login';
+import Profile from './components/Profile';
+
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
 
 const App = (props) => {
   return(
+    <Router>
     <div>
-      HomePage
+      <Header />
+      <NavBar />
+      <Route exact path="/movies" component={Row} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/profile" component={Profile} />
     </div>
+  </Router>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from './components/Header';
-import NavBar from './components/NavBar';
-import Row from './components/Row';
-import Login from './components/Login';
-import Profile from './components/Profile'
-
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import App from './App'
 
 //routes for each main component
 ReactDOM.render((
-  <Router>
-    <div>
-      <Header />
-      <NavBar />
-      <Route exact path="/movies" component={Row} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/profile" component={Profile} />
-    </div>
-  </Router>),
+  <App />
+  ),
   document.getElementById('root')
 );
